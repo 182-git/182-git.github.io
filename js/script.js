a6139de1d04020bad4a7b6a430f2f7ecc92e7e3f
182吉特 <1827045356@qq.com>
2020-09-24 12:05:13 +0800

    Site updated: 2020-09-24 12:05:07

--- js/script.js
+++ js/script.js
@@ -29,6 +29,29 @@
         }
       });
   });
+  
+  
+  //打字机
+  // <script src="node_modules/typed.js/lib"></script> //引入js文件
+  // var options = {
+  //     strings: ['生活', '啊'],
+  //     typeSpeed: 120, //打印的速度。数值越大，速度越慢
+  //     loop: true, //开启循环
+  //     loopCount: Infinity, //设置循环次数，值为：Infinity，也可以使用数值
+  //     backSpeed: 20, //延迟时间后才后退，值越大，延迟的时间就越长
+  //     cursorChar: '^' //光标的符号，比如：
+  // };
+  // var typed = new Typed('#description', options);
+
+
+  //title切换
+      window.onblur=function(){        
+          document.title="你走了~我好伤心┭┮﹏┭┮";    
+          };    
+          window.onfocus=function () {        
+          document.title="感谢经历";   
+          }
+
 
 //返回顶部
   window.onscroll = function() {scrollFunction()};
